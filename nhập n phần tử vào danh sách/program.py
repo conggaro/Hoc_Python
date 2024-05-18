eca248e00609395346d6cb97534686b53be3f70b
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-05-18 13:18:19 +0700

    cap nhat

---
+++ nhập n phần tử vào danh sách/program.py
@@ -0,0 +1,39 @@
+# nhập số
+text = input("Nhập số lượng phần tử: ")
+n = int(text)
+
+
+# tạo danh sách
+arr = []
+
+
+# xuống dòng
+print()
+
+
+# nhập giá trị cho n phần tử
+for index in range(0, n):
+    message = "Nhập giá trị cho phần tử arr[" + str(index) + "] = "
+    itemText = input(message)
+    itemNumber = int(itemText)
+    arr.append(itemNumber)
+
+
+# in giá trị đã nhập ra màn hình
+print("\nDanh sách phần tử vừa nhập:")
+
+for index in range(0, len(arr)):
+    print(arr[index], end = " ")
+
+
+# tính tổng
+total = 0
+
+for item in arr:
+    total = total + item
+
+print("\n\nTổng = " + str(total))
+
+
+# tính trung bình cộng
+print("\nTrung bình cộng = " + str(total / n))
